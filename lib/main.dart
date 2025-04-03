8741cc2846e2a0d5a5b22328a4a358f94ac75f29
bammyu <mbjun3630@naver.com>
2025-04-03 13:06:25 +0900

    ProviderObserver Logger 사용

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_lv2_riverpod/riverpod/provider_observer.dart';
 import 'package:flutter_lv2_riverpod/screen/home_screen.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 void main() {
   runApp(
-    const ProviderScope(
-      child: MaterialApp(
+    ProviderScope(
+      observers: [
+        Logger(),
+      ],
+      child: const MaterialApp(
         home: HomeScreen(),
       ),
     ),
