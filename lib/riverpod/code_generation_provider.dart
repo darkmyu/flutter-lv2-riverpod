168a30613de474edc907491ab13ab498c809e520
bammyu <mbjun3630@naver.com>
2025-04-03 18:38:07 +0900

    Riverpod v2 keepAlive 속성 사용

--- lib/riverpod/code_generation_provider.dart
+++ lib/riverpod/code_generation_provider.dart
@@ -6,3 +6,19 @@ part 'code_generation_provider.g.dart';
 String gState(GStateRef ref) {
   return 'Hello Code Generation';
 }
+
+@riverpod
+Future<int> gStateFuture(GStateFutureRef ref) async {
+  await Future.delayed(const Duration(seconds: 3));
+
+  return 10;
+}
+
+@Riverpod(
+  keepAlive: true,
+)
+Future<int> gStateFuture2(GStateFuture2Ref ref) async {
+  await Future.delayed(const Duration(seconds: 3));
+
+  return 10;
+}
--- lib/screen/code_generation_screen.dart
+++ lib/screen/code_generation_screen.dart
@@ -1,4 +1,4 @@
-import 'package:flutter/widgets.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter_lv2_riverpod/layout/default_layout.dart';
 import 'package:flutter_lv2_riverpod/riverpod/code_generation_provider.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
@@ -9,12 +9,39 @@ class CodeGenerationScreen extends ConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     final state1 = ref.watch(gStateProvider);
+    final state2 = ref.watch(gStateFutureProvider);
+    final state3 = ref.watch(gStateFuture2Provider);
 
     return DefaultLayout(
       title: 'CodeGenerationScreen',
       body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
         children: [
           Text('State1 : $state1'),
+          state2.when(
+            data: (data) => Text(
+              'State2 : $data',
+              textAlign: TextAlign.center,
+            ),
+            error: (err, stack) => Text(
+              err.toString(),
+            ),
+            loading: () => const Center(
+              child: CircularProgressIndicator(),
+            ),
+          ),
+          state3.when(
+            data: (data) => Text(
+              'State3 : $data',
+              textAlign: TextAlign.center,
+            ),
+            error: (err, stack) => Text(
+              err.toString(),
+            ),
+            loading: () => const Center(
+              child: CircularProgressIndicator(),
+            ),
+          ),
         ],
       ),
     );
